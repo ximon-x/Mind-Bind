232899e2f272641ebae0e651f1988b12eaac10ef
Simon Samuel <salviensky@gmail.com>
2024-01-02 00:23:03 +0100

    chore: project initialization

---
+++ src/test/java/com/ximon_x/app/AppTest.java
@@ -0,0 +1,20 @@
+package com.ximon_x.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
